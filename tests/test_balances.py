2a7aa5a5bb02c6d75f4572a7e3aa05e7a50266b7
3Ring <celestialswashbuckler@gmail.com>
2022-04-18 12:51:10 -0400

    negative tests

--- tests/test_balances.py
+++ tests/test_balances.py
@@ -26,7 +26,77 @@ def test_balance_is_accurate(mock: FlaskClient):
         assert ret.status_code == 200
         check(ret3.get_json(), assets["user"])
 
+
 def check(balance: dict, user: dict):
     for k, v in user.items():
         if k != "id" and k != "client_id" and v != 0.0:
             assert balance["balances"][k] == v
+
+
+# negative tests
+def test_invalid_methods(mock: FlaskClient):
+    print("test_invalid_methods")
+    assets = fill_mock()
+    headers = {
+        "client_id": assets["client"]["id"],
+        "authorization": assets["client"]["key"],
+    }
+    url = f"/balances/{assets['user']['id']}"
+    for method in ["post", "put", "delete"]:
+        call = getattr(mock, method)
+        ret = call(url, headers=headers)
+        assert ret.status_code == 405
+        assert b"method is not allowed" in ret.data
+
+
+def test_missing_headers(mock: FlaskClient):
+    print("test_missing_headers")
+    assets = fill_mock()
+    headers_list = [
+        {},
+        {"client_id": assets["client"]["id"]},
+        {"authorization": assets["client"]["key"]},
+    ]
+    url = f"/balances/{assets['user']['id']}"
+    for headers in headers_list:
+        ret = mock.get(url, headers=headers)
+        assert ret.status_code == 400
+        assert b"header required" in ret.data
+
+
+def test_missing_args(mock: FlaskClient):
+    print("test_missing_args")
+    assets = fill_mock()
+    headers = {
+        "client_id": assets["client"]["id"],
+        "authorization": assets["client"]["key"],
+    }
+    url = f"/balances/"
+    ret = mock.post(url, headers=headers)
+    assert ret.status_code == 404
+
+
+def test_user_does_not_exist(mock: FlaskClient):
+    print("test_user_does_not_exist")
+    assets = fill_mock()
+    headers = {
+        "client_id": assets["client"]["id"],
+        "authorization": assets["client"]["key"],
+    }
+    url = f"/balances/{assets['user']['id'] + 1}"
+    ret = mock.get(url, headers=headers)
+    assert ret.status_code == 404
+
+
+def test_user_does_not_belong_to_client(mock: FlaskClient):
+    print("test_user_does_not_belong_to_client")
+    assets1 = fill_mock()
+    assets2 = fill_mock()
+    headers = {
+        "client_id": assets2["client"]["id"],
+        "authorization": assets1["client"]["key"],
+    }
+    url = f"/balances/{assets1['user']['id']}"
+    ret = mock.get(url, headers=headers)
+    assert ret.status_code == 401
+    assert b"unauthorized client" in ret.data
